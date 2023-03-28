55ac190fcc67447db2a14e8a133e0e7555a958c7
Ankurit Bhakta <ankuritbhakta1891@gmail.com>
2023-03-28 11:38:18 +0530

    Option dialog dark theme

--- src/components/OptionDialog.tsx
+++ src/components/OptionDialog.tsx
@@ -6,6 +6,31 @@ import { GoCircleSlash } from "react-icons/go";
 import "./OptionDialog.css";
 
 const OptionDialog = (props: any) => {
+  const listTitleStyle: any = {
+    paddingLeft: "30px",
+    paddingBottom: "10px",
+    paddingTop: "10px",
+    color: props.theme ? "#252424e6" : "#FFFFFFDC",
+    fontSize: "14px",
+    cursor: "pointer",
+    marginBottom: "1px",
+  };
+
+  const mainBoxStyle: any = {
+    top: props.xCords - 50,
+    left: props.yCords,
+    backgroundColor: props.theme ? "#f3f2f1" : "#000000",
+    position: "absolute",
+    zIndex: "200",
+    borderRadius: "2px",
+    listStyle: "none",
+    width: "200px",
+    boxShadow: props.theme
+      ? "1px 1px 4px -1px #252424"
+      : "1px 1px 4px -1px #FFFFFF",
+    height: "fit-content",
+  };
+
   const onAddStartHandler = () => {
     props.setOpenDialog((val: any) => !val);
     props.assignNodeValues("#656AC69F", <VscDebugStart />, "input", "Start");
@@ -26,39 +51,50 @@ const OptionDialog = (props: any) => {
     props.assignNodeValues("#000000", <GoCircleSlash />, "output", "Finish");
   };
   return (
-    <div
-      className="main-box"
-      style={{
-        top: props.xCords - 50,
-        left: props.yCords,
-        backgroundColor: "#f3f2f1",
-        color: "black",
-      }}
-    >
+    <div className="main-box" style={mainBoxStyle}>
       <ul className="list-class">
         <motion.li
           className="list-title"
           onClick={onAddStartHandler}
-          initial={{ backgroundColor: "#f3f2f1" }}
-          whileHover={{ backgroundColor: "#E9E3DE" }}
+          initial={{ backgroundColor: props.theme ? "#f3f2f1" : "#000000" }}
+          whileHover={{ backgroundColor: props.theme ? "#E9E3DE" : "#141414" }}
+          style={listTitleStyle}
         >
           Add Start
         </motion.li>
-        <hr />
+        <hr
+          style={{
+            color: "rgb(169, 171, 173)",
+            opacity: "0.25",
+            margin: "0px",
+            border: "0",
+            borderTop: "var(--bs-border-width) solid",
+          }}
+        />
         <motion.li
           className="list-title"
           onClick={onAddDefaultHandler}
-          initial={{ backgroundColor: "#f3f2f1" }}
-          whileHover={{ backgroundColor: "#E9E3DE" }}
+          initial={{ backgroundColor: props.theme ? "#f3f2f1" : "#000000" }}
+          whileHover={{ backgroundColor: props.theme ? "#E9E3DE" : "#141414" }}
+          style={listTitleStyle}
         >
           Add State
         </motion.li>
-        <hr />
+        <hr
+          style={{
+            color: "rgb(169, 171, 173)",
+            opacity: "0.25",
+            margin: "0px",
+            border: "0",
+            borderTop: "var(--bs-border-width) solid",
+          }}
+        />
         <motion.li
           className="list-title"
           onClick={onAddTerminateHandler}
-          initial={{ backgroundColor: "#f3f2f1" }}
-          whileHover={{ backgroundColor: "#E9E3DE" }}
+          initial={{ backgroundColor: props.theme ? "#f3f2f1" : "#000000" }}
+          whileHover={{ backgroundColor: props.theme ? "#E9E3DE" : "#141414" }}
+          style={listTitleStyle}
         >
           Add Finish
         </motion.li>
