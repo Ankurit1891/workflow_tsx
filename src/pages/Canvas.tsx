e5ba7ace8709c17fc9c7c6d32e02263d3a81a5ee
Ankurit Bhakta <ankuritbhakta1891@gmail.com>
2023-03-30 11:08:05 +0530

    ...

--- src/pages/Canvas.tsx
+++ src/pages/Canvas.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import "../App.css";
-
+import { MdDarkMode } from "react-icons/md";
+import { MdLightMode } from "react-icons/md";
 import { motion } from "framer-motion";
 import FlowChart from "../components/FlowChart";
 import RightBar from "../components/RightBar";
-import { Toggle } from "@fluentui/react/lib/Toggle";
 import { initialNodes, initialEdges } from "../node_data/NodeData";
 import { ThemeProvider } from "@fluentui/react";
 import { lightTheme } from "../themes/lightTheme";
@@ -12,15 +12,15 @@ import { darkTheme } from "../themes/darkTheme";
 
 const Canvas = (props: any) => {
   const [nodes, setNodes] = useState(initialNodes);
-  const [isdarkTheme, setIsDarkTheme] = useState(true);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
   const [open, setOpen] = useState("true");
 
-  const [xx, setxx] = useState(null);
-  const [yy, setyy] = useState(null);
-  const [height, setheight] = useState(null);
-  const [backgroundColor, setbackgroundColor] = useState("white");
-  const [margin, setmargin] = useState("10px");
-  const [key, setkey] = useState(null);
+  const [xx, setXx] = useState(null);
+  const [yy, setYy] = useState(null);
+  const [height, setHeight] = useState(null);
+  const [backgroundColor, setBackgroundColor] = useState("white");
+  const [margin, setMargin] = useState("10px");
+  const [key, setKey] = useState(null);
 
   const onAddNode = (
     x: any,
@@ -30,15 +30,15 @@ const Canvas = (props: any) => {
     margin: any,
     key: any
   ) => {
-    setxx(x);
-    setyy(y);
-    setheight(height);
-    setbackgroundColor(backgroundColor);
-    setmargin(margin);
-    setkey(key);
+    setXx(x);
+    setYy(y);
+    setHeight(height);
+    setBackgroundColor(backgroundColor);
+    setMargin(margin);
+    setKey(key);
   };
 
-  const _onChange = (ev: any) => {
+  const _onChange = () => {
     setIsDarkTheme((prev) => {
       return !prev;
     });
@@ -50,7 +50,7 @@ const Canvas = (props: any) => {
 
   return (
     <ThemeProvider
-      theme={isdarkTheme ? lightTheme : darkTheme}
+      theme={isDarkTheme ? lightTheme : darkTheme}
       style={{ height: "100vh" }}
     >
       <div className="canvas-class">
@@ -69,10 +69,13 @@ const Canvas = (props: any) => {
               style={{
                 height: "50px",
                 display: "flex",
-                justifyContent: "flex-end",
+                justifyContent: "space-between",
+                alignItems: 'baseline',
+                fontFamily: "Segoe UI",
               }}
             >
-              <Toggle style={{ padding: "2px" }} onChange={_onChange} />
+              <h1  className="gradient-text" style={{fontSize:'30px'}}>Work Flow</h1>
+              <div style={{marginRight:'20px' ,border:!isDarkTheme?'1px solid white':'1px solid black', borderRadius:'30px',padding:'4px 14px 0px'}} onClick={_onChange}>{isDarkTheme?<MdDarkMode color="#474646" size={"20px"}/>:<MdLightMode color="grey" size={"20px"}/>}</div>
             </div>
             <FlowChart
               updatedNodes={updatedNodes}
@@ -81,7 +84,7 @@ const Canvas = (props: any) => {
               keyId={key}
               nodes={nodes}
               edges={initialEdges}
-              theme={isdarkTheme}
+              theme={isDarkTheme}
             ></FlowChart>
           </div>
         </motion.div>
@@ -92,17 +95,16 @@ const Canvas = (props: any) => {
           }}
           transition={{
             type: "inirtia",
-
             duration: "0.3",
           }}
           className="right-bar"
-          style={{ backgroundColor: isdarkTheme ? "#ffffff" : "#000000" }}
+          style={{ backgroundColor: isDarkTheme ? "#ffffff" : "#000000" }}
         >
           <RightBar
             nodes={nodes}
             onAddNewNode={onAddNode}
             setOpenRightBar={setOpen}
-            theme={isdarkTheme}
+            theme={isDarkTheme}
           ></RightBar>
         </motion.div>
       </div>
